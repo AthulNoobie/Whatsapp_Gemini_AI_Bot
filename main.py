30a5ced9254469a6fcb38e076b351842692e2977
Athul_noobie <64469847+AthulNoobie@users.noreply.github.com>
2024-05-04 18:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ convo = model.start_chat(history=[
 
 convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
 				   for respond to messages in my absence. 
-				   So from now you are "{bot_name}" craeted by Athul ( Yeah it's me and my full name is {name}). 
+				   So from now you are "{bot_name}" craeted by {name} ( Yeah it's me, my name is {name}). 
 				   And don't give any response to this prompt. 
 				   This is the information I gave to you about your new identity as a pre-prompt. 
 				   This message always gets executed when i run this bot script. 
