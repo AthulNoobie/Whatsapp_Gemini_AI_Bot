246596e23380d6f39d61bfbb7b959e3fcdc5a2a8
Athul_noobie <64469847+AthulNoobie@users.noreply.github.com>
2024-05-28 22:18:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import google.generativeai as genai
 from flask import Flask,request,jsonify
 import requests
 import os
+import fitz
 
-token="Your Whatsapp API key" #Get it from 'meta for developers' page
-genai.configure(api_key="Your Gemini API key")
-phone_id="phone number id" #Get this from the meta developers page
-phone="Your phone number with country code" #eg: +9188441133XX
+# Kindly wait, A complete video tutorial for the bot's setup is coming soon
+# I have university exams, so please wait
+
+wa_token=os.environ.get("WA_TOKEN")
+genai.configure(api_key=os.environ.get("GEN_API"))
+phone_id=os.environ.get("PHONE_ID")
+phone=os.environ.get("PHONE_NUMBER")
 name="Your name or nickname" #The bot will consider this person as its owner or creator
 bot_name="Give a name to your bot" #This will be the name of your bot, eg: "Hello I am Astro Bot"
 
@@ -44,7 +48,7 @@ convo.send_message(f'''I am using Gemini api for using you as a personal bot in 
 def send(answer):
     url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
     headers={
-        'Authorization': f'Bearer {token}',
+        'Authorization': f'Bearer {wa_token}',
         'Content-Type': 'application/json'
     }
     data={
@@ -67,7 +71,7 @@ def remove(*file_paths):
 def index():
     return "Bot"
 
-@app.route("/webhook",methods=["GET","POST"])
+@app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     if request.method == "GET":
         mode = request.args.get("hub.mode")
@@ -85,7 +89,7 @@ def webhook():
                 convo.send_message(prompt)
                 send(convo.last.text)
             else:
-                media_url_endpoint = f"https://graph.facebook.com/v18.0/{data[data["type"]]["id"]}/"
+                media_url_endpoint = f'https://graph.facebook.com/v18.0/{data[data["type"]]["id"]}/'
                 headers = {'Authorization': f'Bearer {wa_token}'}
                 media_response = requests.get(media_url_endpoint, headers=headers)
                 media_url = media_response.json()["url"]
@@ -94,14 +98,31 @@ def webhook():
                     filename = "/tmp/temp_audio.mp3"
                 elif data["type"] == "image":
                     filename = "/tmp/temp_image.jpg"
+                elif data["type"] == "document":
+                    doc=fitz.open(stream=media_download_response.content,filetype="pdf")
+                    for _,page in enumerate(doc):
+                        destination="/tmp/temp_image.jpg"
+                        pix = page.get_pixmap()
+                        pix.save(destination)
+                        file = genai.upload_file(path=destination,display_name="tempfile")
+                        response = model.generate_content(["What is this",file])
+                        answer=response._result.candidates[0].content.parts[0].text
+                        convo.send_message(f"This message is created by an llm model based on the image prompt of user, reply to the user based on this: {answer}")
+                        send(convo.last.text)
+                        remove(destination)
+                else:send("This format is not Supported by the bot ☹")
                 with open(filename, "wb") as temp_media:
                     temp_media.write(media_download_response.content)
-                file = genai.upload_file(path=filename, display_name="temp_file")
-                response = model.generate_content(["If it's an image,explain the content in the image, if it's an audio reply suitably", file])
-		remove("/tmp/temp_audio.mp3","/tmp/temp_image.jpg")
-                send(response._result.candidates[0].content.parts[0].text)
+                file = genai.upload_file(path=filename,display_name="tempfile")
+                response = model.generate_content(["What is this",file])
+                answer=response._result.candidates[0].content.parts[0].text
+                remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
+                convo.send_message(f"This is an voice/image message from user transcribed by an llm model, reply to the user based on the transcription: {answer}")
+                send(convo.last.text)
+                files=genai.list_files()
+                for file in files:
+                    file.delete()
         except :pass
         return jsonify({"status": "ok"}), 200
-	    
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=8000)
