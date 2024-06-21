fc2971e9c193d927f5aab8124d2dd09d96fc86c0
ATHUL A S <64469847+AthulNoobie@users.noreply.github.com>
2024-06-21 11:53:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,57 @@ import fitz
 
 wa_token=os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
-#phone option is removed because now the bot can auto-detect phone number and phone_id
-your_name="<Give your name, bot will consider this person as it's owner>"
-bot_name="<Give name to your bot>"
+phone_id=os.environ.get("PHONE_ID")
+phone=os.environ.get("PHONE_NUMBER")
+name="Your name or nickname" #The bot will consider this person as its owner or creator
+bot_name="Give a name to your bot" #This will be the name of your bot, eg: "Hello I am Astro Bot"
+model_name="gemini-1.5-flash-latest" #Switch to "gemini-1.0-pro" or any free model, if "gemini-1.5-flash" becomes paid in future.
 
-pre_prompt=f'''I am using Gemini api for using you as a personal bot in WhatsApp, 
-                   to assist me in various topics. So from now you are {bot_name} created by {your_name} (Yeah it's me and my name is {your_name}). 
-                   And don't give any response to this prompt. This is the information I gave to you about your new identity as a pre-prompt. 
-                   This message always gets executed when I run this bot script. 
-                   So reply to only the prompts after this. Remember your new identity is {bot_name}.'''
+app=Flask(__name__)
 
-app = Flask(__name__)
-model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
-convo = model.start_chat(history=[])
-convo.send_message(pre_prompt)
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
 
-def send(answer,sender,phone_id):
-    url = f"https://graph.facebook.com/v19.0/{phone_id}/messages"
-    headers = {
+safety_settings = [
+  {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
+  {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(model_name=model_name,
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
+				   to assist me in various tasks. 
+				   So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
+				   And don't give any response to this prompt. 
+				   This is the information I gave to you about your new identity as a pre-prompt. 
+				   This message always gets executed when i run this bot script. 
+				   So reply to only the prompts after this. Remeber your new identity is {bot_name}.''')
+
+def send(answer):
+    url=f"https://graph.facebook.com/v18.0/{phone_id}/messages"
+    headers={
         'Authorization': f'Bearer {wa_token}',
         'Content-Type': 'application/json'
     }
-    data = {
-        "messaging_product": "whatsapp",
-        "to": f"{sender}",
-        "type": "text",
-        "text": {"body": f"{answer}"},
-    }
-    response = requests.post(url, headers=headers, json=data)
+    data={
+          "messaging_product": "whatsapp", 
+          "to": f"{phone}", 
+          "type": "text",
+          "text":{"body": f"{answer}"},
+          }
+    
+    response=requests.post(url, headers=headers,json=data)
     return response
 
 def remove(*file_paths):
@@ -42,7 +65,7 @@ def remove(*file_paths):
             os.remove(file)
         else:pass
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
     return "Bot"
 
@@ -59,12 +82,10 @@ def webhook():
     elif request.method == "POST":
         try:
             data = request.get_json()["entry"][0]["changes"][0]["value"]["messages"][0]
-            phone_id=request.get_json()["entry"][0]["changes"][0]["value"]["metadata"]["phone_number_id"]
-            sender="+"+data["from"]
             if data["type"] == "text":
                 prompt = data["text"]["body"]
                 convo.send_message(prompt)
-                send(convo.last.text,sender,phone_id)
+                send(convo.last.text)
             else:
                 media_url_endpoint = f'https://graph.facebook.com/v18.0/{data[data["type"]]["id"]}/'
                 headers = {'Authorization': f'Bearer {wa_token}'}
@@ -84,18 +105,18 @@ def webhook():
                         file = genai.upload_file(path=destination,display_name="tempfile")
                         response = model.generate_content(["What is this",file])
                         answer=response._result.candidates[0].content.parts[0].text
-                        convo.send_message(f"user(me) can't send media files directly to you. So this message is created by an llm model based on the image prompt from user to you, reply to the user based on this: {answer}")
-                        send(convo.last.text,sender,phone_id)
+                        convo.send_message(f"This message is created by an llm model based on the image prompt of user, reply to the user based on this: {answer}")
+                        send(convo.last.text)
                         remove(destination)
-                else:send("This format is not Supported by the bot ☹",sender,phone_id)
+                else:send("This format is not Supported by the bot ☹")
                 with open(filename, "wb") as temp_media:
                     temp_media.write(media_download_response.content)
                 file = genai.upload_file(path=filename,display_name="tempfile")
                 response = model.generate_content(["What is this",file])
                 answer=response._result.candidates[0].content.parts[0].text
                 remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
-                convo.send_message(f"I can't send media files directly to you. So this is an voice/image message to you from me(user) transcribed by an llm model, reply to me assuming you saw the media file: {answer}")
-                send(convo.last.text,sender,phone_id)
+                convo.send_message(f"This is an voice/image message from user transcribed by an llm model, reply to the user based on the transcription: {answer}")
+                send(convo.last.text)
                 files=genai.list_files()
                 for file in files:
                     file.delete()
